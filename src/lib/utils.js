99f0766e7d6fea41cc067d56f0873634cdbc03e8
TyJacalan <muzuuu@protonmail.com>
2023-11-18 17:47:32 +0800

    feat: add function to filter active messages

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,23 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getFriendsList(messages) {
+  return Array.from(
+    new Set(
+      messages.map((message) => {
+        const emailWithoutDomain = message.sender.uid.split("@")[0];
+
+        return JSON.stringify({
+          id: message.sender.id,
+          uid: message.sender.uid,
+          name: emailWithoutDomain,
+        });
+      })
+    ),
+    JSON.parse
+  );
 }
