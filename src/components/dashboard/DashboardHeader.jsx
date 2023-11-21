723da4acfaa8fffb435a7f6cb3ac15d9bf78dd90
TyJacalan <muzuuu@protonmail.com>
2023-11-21 21:14:52 +0800

    feat: convert div to link for logo redirection to home page

--- src/components/dashboard/DashboardHeader.jsx
+++ src/components/dashboard/DashboardHeader.jsx
@@ -1,15 +1,16 @@
-import logo from "@public/logo.png";
+import { Link } from "react-router-dom";
 
+import logo from "@public/logo.png";
 import ModeToggle from "../ui/ModeToggle";
 import { Input } from "@/components/ui/input";
 
 export default function Header() {
   return (
     <div className="flex flex-row items-center justify-around gap-4 h-8 w-full py-1">
-      <div className="flex flex-row w-12 sm:w-48">
+      <Link to="/" className="flex flex-row w-12 sm:w-48">
         <img src={logo} alt="Rooms Logo" className="h-6 w-auto" />
         <span className="hidden sm:block">Rooms</span>
-      </div>
+      </Link>
       <div className="flex-1">
         <Input
           className="max-w-sm"
