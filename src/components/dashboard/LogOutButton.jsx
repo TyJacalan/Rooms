1e72446bba509237eb1e459a5689c199119d70c5
TyJacalan <muzuuu@protonmail.com>
2023-11-24 15:41:10 +0800

    feat: add redirect to sign-in page

--- src/components/dashboard/LogOutButton.jsx
+++ src/components/dashboard/LogOutButton.jsx
@@ -1,10 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "@/store/contexts/authContext";
 
 export default function LogOutButton() {
   const { logOutAction } = useAuthContext();
-
+  const navigate = useNavigate();
   async function handleLogOut() {
     await logOutAction();
+    navigate("/signin");
   }
 
   return <div onClick={handleLogOut}>Log Out</div>;
