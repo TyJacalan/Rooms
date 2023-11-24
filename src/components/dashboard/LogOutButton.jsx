1751e7c67cbf64c0d3ebe0edcbf60357bd5c00a9
TyJacalan <muzuuu@protonmail.com>
2023-11-24 22:28:44 +0800

    feat: add loading state

--- src/components/dashboard/LogOutButton.jsx
+++ src/components/dashboard/LogOutButton.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "@/store/contexts/authContext";
+
+import { Loader2 } from "lucide-react";
 
 export default function LogOutButton() {
   const { logOutAction } = useAuthContext();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState();
+
   async function handleLogOut() {
+    setIsLoading(true);
     await logOutAction();
+    setIsLoading(false);
     navigate("/signin");
   }
 
-  return <div onClick={handleLogOut}>Log Out</div>;
+  return (
+    <div onClick={handleLogOut} disabled={isLoading}>
+      {isLoading ? <Loader2 /> : "Log Out"}
+    </div>
+  );
 }
