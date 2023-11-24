c1ac0891de1f0d7a2526fd5c5d0ec8319d96b3c1
TyJacalan <muzuuu@protonmail.com>
2023-11-24 18:32:00 +0800

    chore: remove searchbar

--- src/components/dashboard/DashboardHeader.jsx
+++ src/components/dashboard/DashboardHeader.jsx
@@ -2,22 +2,14 @@ import { Link } from "react-router-dom";
 
 import logo from "@public/logo.png";
 import ModeToggle from "../ui/ModeToggle";
-import { Input } from "@/components/ui/input";
 
 export default function Header() {
   return (
-    <div className="flex flex-row items-center justify-around gap-4 h-8 w-full py-1">
+    <div className="flex flex-row items-center justify-between gap-4 h-8 w-full py-1">
       <Link to="/" className="flex flex-row w-12 sm:w-48">
         <img src={logo} alt="Rooms Logo" className="h-6 w-auto" />
         <span className="hidden sm:block">Rooms</span>
       </Link>
-      <div className="flex-1">
-        <Input
-          className="max-w-sm"
-          type="email"
-          placeholder="Search chats and rooms..."
-        />
-      </div>
 
       <div>
         <ModeToggle />
