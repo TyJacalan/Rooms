028c191972480a5d1165d4b75593b15dfdc5aa0a
TyJacalan <muzuuu@protonmail.com>
2023-11-24 22:47:52 +0800

    hotfix: correct case for componentContext path

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import AuthProvider from "@/store/contexts/authContext";
-import ComponentProvider from "@/store/contexts/ComponentContext.jsx";
+import ComponentProvider from "@/store/contexts/componentContext.jsx";
 import MessagesProvider from "@/store/contexts/messagesContext";
 import ThemeProvider from "@/store/contexts/themeContext";
 
